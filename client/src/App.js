dbb96497bc575ab1b783fdb957e9b5d224346c2d
0oAVIRALo0 <aviral21029@iiitd.ac.in>
2023-12-28 15:09:15 +0530

    Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,45 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Forms/Form";
+import memories from "./images/memories.png";
+import useStyles from "./styles";
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>App</h1>
-    </div>
-  )
-}
+    <Container maxwidth="lg">
+      <AppBar className={classes.appBar} cposition="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memmories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
-import express from 'express'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
-const app = express() 
+import postRoutes from "./routes/posts.js";
 
-app.use(bodyParser.json({limit: "30mb", extended: true}))
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
-app.use(cors())
+const app = express();
 
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://aviral:aviral123@cluster0.w7h8loc.mongodb.net/";
+const PORT = process.env.PORT || 5005;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() => {
+    // console.log("Database connected");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((error) => console.log(error.message));
+
+// mongoose.set("useFindAndModify", false);
