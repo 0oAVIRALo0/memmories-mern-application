e999f07603e83326036aa0f4a69c7c22dd1afb26
0oAVIRALo0 <aviral21029@iiitd.ac.in>
2023-12-28 23:40:18 +0530

    Will continue later

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Forms/Form";
@@ -8,6 +9,11 @@ import useStyles from "./styles";
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPosts());
+  }, []);
+
   return (
     <Container maxwidth="lg">
       <AppBar className={classes.appBar} cposition="static" color="inherit">
--- client/src/components/Forms/Form.js
+++ client/src/components/Forms/Form.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
+
 import useStyles from "./styles";
 
 const Form = () => {
   const classes = useStyles();
-  return <h1>Form</h1>;
+
+  const handleSubmit = () => {};
+
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={classes.form}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Creating a memmory</Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          onChange={}
+        ></TextField>
+      </form>
+    </Paper>
+  );
 };
 
 export default Form;
